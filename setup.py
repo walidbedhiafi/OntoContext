12b41632e81077211564fc09f0fb36070e84dee4
walidbedhiafi <ontocontext@gmail.com>
2016-08-21 03:59:46 +0200

    Update setup.py

---
+++ setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(
+    name = 'ontocontext',
+    packages = ['ontocontext'],
+    version = '0.1',  
+    description = 'Beta Version',
+    author = 'Walid Bedhiafi',
+    author_email = 'ontocontext@gmail.com',
+    url = 'https://github.com/walidbedhiafi/OntoContext',
+    download_url = 'https://github.com/walidbedhiafi/OntoContext/archive/0.1.tar.gz',
+    keywords = ['Bioinformatics', 'textmining' , 'Annotating', 'Ontology', 'biomedicaltext'],
+    classifiers = [],
+)
